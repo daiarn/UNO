73b1d478033ddf85aa0e079fc46cdb19828b7f9c
jRytis <rytis.janceris@ktu.edu>
2019-09-24 10:12:20 +0300

    added new classes

---
+++ UNO/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO.Models
+{
+    class Game
+    {
+        private DateTime gameTime { get; set; }
+        private Boolean flowClockWise { get; set; }
+        private Deck deck { get; set; }
+
+    }
+}
---
+++ UNO/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO.Models
+{
+    class Player
+    {
+        private String username { get; set; }
+
+        private Hand hand { get; set; }
+    }
+}

d4e2e80e9cca229062d84b2aff8dc59a3d44a76d
daiarn <dainius1997@gmail.com>
2019-09-27 21:23:59 +0300

    Added observer class

---
+++ UNO/Patterns/Observer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO.Models
+{
+    public abstract class Observer
+    {
+        protected Game Game;
+        public abstract void Update(Card card);
+    }
+}

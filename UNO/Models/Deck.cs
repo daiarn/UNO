353df4c276c4a1d96926729a6c3cfdb2a215f5eb
linsas0109 <linsas0109@gmail.com>
2019-09-29 21:05:21 +0300

    Builder class

--- UNO/Models/Deck.cs
+++ UNO/Models/Deck.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace UNO.Models
 {
-    class Deck
-    {
-        public Card[] cards { get; set; }
+	class Deck
+	{
+		private List<Card> cards { get; set; }
 
+		public void add(Card card)
+		{
+			cards.Add(card);
+		}
 
-    }
+		public void shuffle()
+		{
+			var arr = cards.ToArray();
+			var rand = new Random();
+
+			for (int i = arr.Length - 1; i > 0; i--)
+			{
+				int j = rand.Next(i + 1);
+
+				var temp = arr[j];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+
+			cards = arr.ToList();
+		}
+	}
 }

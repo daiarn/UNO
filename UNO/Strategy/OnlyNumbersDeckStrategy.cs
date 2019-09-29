021d268faa83329f0ed6e9b5654eb763447c6d19
jRytis <rytis.janceris@ktu.edu>
2019-09-29 21:53:45 +0300

    Added aditional strategy classes

---
+++ UNO/Strategy/OnlyNumbersDeckStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UNO.Models;
+
+namespace UNO.Strategy
+{
+    class OnlyNumbersDeckStrategy : GameModeStrategy
+    {
+        public Deck getDect(Deck playDeck, Deck usedDeck)
+        {
+            playDeck.RemoveActionAndWildCards();
+            return playDeck;
+        }
+    }
+}

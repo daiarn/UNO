91407ffebde59b43687b84d638ea453d02138d01
daiarn <dainius1997@gmail.com>
2019-10-18 23:56:44 +0300

    lobby shoes players info lets you choose game mode and does start game post request

--- UNO_Client/Forms/LobyForm.cs
+++ UNO_Client/Forms/LobyForm.cs
@@ -1,25 +1,78 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UNO_Client.Models;
 
 namespace UNO_Client.Forms
 {
     public partial class LobyForm : Form
     {
-        public LobyForm()
+        private const string BASE_URL = "https://localhost:44331/api/game"; //TODO: change this
+        private static readonly HttpClient client = new HttpClient();
+        private JoinPost joinPost;
+        private Game Game;
+        public LobyForm(JoinPost joinPost)
         {
-            InitializeComponent();
+            this.joinPost = joinPost;
+            SetGame();
+            //ShowPlayersInformation();
         }
 
         private void LobyForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            bool isOnlyNumbers = gameOptions.GetItemChecked(0);
+            bool isFiniteDeck = gameOptions.GetItemChecked(1);
+            //Start game
+            string JsonString = "{\"finiteDeck\":\"" + isFiniteDeck +
+                "\"onlyNumbers\":\"" + isOnlyNumbers +
+                "\"}";
+            var content = new StringContent(JsonString, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(BASE_URL + "/start", content);
+            GameForm form = new GameForm(joinPost);
+            this.Close();
+            form.Show();
+        }
+
+        private void gameOptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void SetGame()
+        {
+            var respondeString = await client.GetStringAsync(BASE_URL + "/" + joinPost.Id);
+            //json serializer to Game object and set it globaly
+            Game = JsonConvert.DeserializeObject<Game>(respondeString);
+            InitializeComponent();
+        }
+        private string[] FormatPlayersInformation()
+        {
+            int count = Game.Gamestate.Players.Count;
+            string[] infomration = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                var player = Game.Gamestate.Players[i];
+                infomration[i] = player.Name;
+            }
+            return infomration;
+        }
+        private void ShowPlayersInformation()
+        {
+            var info = FormatPlayersInformation();
+            Players.Lines = info;
         }
     }
 }

d20984ff181d757015adf39e31119e87e0ac14ea
daiarn <dainius1997@gmail.com>
2019-10-18 23:53:58 +0300

    Added new model for response message from join post

---
+++ UNO_Client/Models/JoinPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO_Client.Models
+{
+    public class JoinPost
+    {
+        bool success;
+        string id;
+
+        public bool Success { get => success; set => success = value; }
+        public string Id { get => id; set => id = value; }
+    }
+}

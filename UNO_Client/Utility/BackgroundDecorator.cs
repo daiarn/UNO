dff595681c3137b72ba52330fe433aa70c62eefc
daiarn <dainius1997@gmail.com>
2019-10-21 20:39:31 +0300

    Added needed classes for decorator pattern

---
+++ UNO_Client/Utility/BackgroundDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace UNO_Client.Utility
+{
+    public class BackgroundDecorator : RectDecorator
+    {
+        public BackgroundDecorator(Rect rectangleToBeDecorated) : base(rectangleToBeDecorated)
+        {
+            this.rectangle = rectangleToBeDecorated.rectangle;
+            this.graphics = rectangleToBeDecorated.graphics;
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+            SolidBrush WhiteBrush = new SolidBrush(Color.White);
+            rectangleToBeDecorated.graphics.FillRectangle(WhiteBrush, rectangleToBeDecorated.rectangle);
+        }
+    }
+}

527811217f97bb06426f321cf06a5b2284a6182d
daiarn <dainius1997@gmail.com>
2019-10-30 16:19:35 +0200

    Added undo card tests

---
+++ UNO_Tests/UndoCardTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UNO_Server.Controllers;
+using UNO_Server.Models;
+using UNO_Server.Models.RecvData;
+namespace UNO_Tests
+{
+    [TestClass]
+    public class UndoCardTests
+    {
+        [TestMethod]
+        public void TestUndoDrawCard()
+        {
+            // ARRANGE
+            var game = Game.ResetGame();
+            var control = new GameController();
+
+            game.AddPlayer("Player One");
+            game.AddPlayer("Player Two");
+            game.phase = GamePhase.Playing;
+            game.drawPile = new Deck();
+            game.drawPile.AddToBottom(new Card(CardColor.Red, CardType.Zero));
+            game.activePlayerIndex = 0;
+            game.players[0].hand.Add(new Card(CardColor.Black, CardType.Wild));
+
+            // ASSERT
+            game.UndoDrawCard(0);
+
+            Assert.AreEqual(0, game.players[0].hand.Count);
+            Assert.AreEqual(2, game.drawPile.GetCount());
+        }
+    }
+}

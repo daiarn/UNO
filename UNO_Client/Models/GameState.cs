401ba60562943f561dd8f83ae3fd72f4687ef889
daiarn <dainius1997@gmail.com>
2019-11-13 15:35:03 +0200

    Added winnerInfo model to ClientSide

--- UNO_Client/Models/GameState.cs
+++ UNO_Client/Models/GameState.cs
@@ -18,6 +18,7 @@ namespace UNO_Client.Models
         Card activeCard;
         List<Player> players;
         List<Card> hand;
+        WinnerInfo[] winners;
 
         public int ZeroCounter { get => zeroCounter; set => zeroCounter = value; }
         public int WildCounter { get => wildCounter; set => wildCounter = value; }
@@ -29,5 +30,6 @@ namespace UNO_Client.Models
         public List<Player> Players { get => players; set => players = value; }
         public List<Card> Hand { get => hand; set => hand = value; }
         public int Index { get => index; set => index = value; }
+        internal WinnerInfo[] Winners { get => winners; set => winners = value; }
     }
 }

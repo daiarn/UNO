c1df3546f4a7b0f10d8b8d407c580d22dd01a185
daiarn <dainius1997@gmail.com>
2019-11-17 21:09:23 +0200

    Addrd Iterator and Agregator interfaces

---
+++ UNO_Server/Utility/Iterator/IAggregate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UNO_Server.Utility.Iterator
+{
+    public interface IAggregate
+    {
+        IIterator GetIterator();
+        object this[int itemIndex] { set; get; }
+        int Count { get; }
+    }
+}
---
+++ UNO_Server/Utility/Iterator/IIterative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace UNO_Server.Utility.Iterator
+{
+    public interface IIterator
+    {
+        object First { get; }
+        object Next { get; }
+        object Current { get; }
+        bool HasNext { get; }
+    }
+}

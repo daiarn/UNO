b8cb9b7e44e1515d9c8bcf8dfc74cc346f015901
daiarn <dainius1997@gmail.com>
2019-11-30 12:46:13 +0200

    Added needed classes for interpreter pattern

---
+++ UNO_Client/Interpreter/Parser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNO_Client.Interpreter
+{
+    class Parser : Expression
+    {
+        List<Expression> tree;
+
+        public Parser()
+        {
+            // Build the 'parse tree'
+            tree = new List<Expression>();
+            tree.Add(new ThousandExpression());
+            tree.Add(new HundredExpression());
+            tree.Add(new TenExpression());
+            tree.Add(new OneExpression());
+        }
+        public override void Interpret(Context context)
+        {
+            // Interpret
+            foreach (Expression exp in tree)
+            {
+                exp.Interpret(context);
+            }
+        }
+
+        public override string Five()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Four()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Multiplier()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Nine()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string One()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
